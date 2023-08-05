36eadc4a9191ef50cd0953a31e4675f36d18d7df
Your Name <you@example.com>
2023-08-05 11:44:16 -0400

    commited on 5 aug 2023

---
+++ src/test/java/com/GMOwebsite/LaunchGmo.java
@@ -0,0 +1,63 @@
+package com.GMOwebsite;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class LaunchGmo {
+  @Test
+  public void TestGmo() {
+	  WebDriverManager.chromedriver().setup();
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("http://demo.borland.com/gmopost/");
+	  driver.manage().deleteAllCookies();
+	  driver.manage().window().maximize();
+	  System.out.println("Inside GMO website");
+	  String title=driver.getTitle();
+	  Assert.assertEquals(title, "Welcome t Green Mountain Outpost");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
